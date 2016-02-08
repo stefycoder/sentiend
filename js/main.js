909d04a531f21c91ca02d5df973e3ae6f8ec7897
JanetHugarcia <nayavz@gmail.com>
2016-02-08 17:56:37 -0500

    persona y home terminado

---
+++ js/main.js
@@ -0,0 +1,17 @@
+
+
+
+/*
+(function($)){
+	var x=0;
+	var y=0;
+
+	var banner=$(".banner");
+
+	banner.css('backgroundPosition',x+ 'px'+' '+y+ 'px');
+
+	window.setInterval(function()){
+		banner.css("backgroundPosition",x+'px' +' '+ y+ 'px');
+		y--;
+	}
+}*/
